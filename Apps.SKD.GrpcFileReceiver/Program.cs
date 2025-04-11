cc78e8bf478f133ccfc5cf0b3d4510bc266d9226
송경동(SKD-NOTE6) <kdsong69@gmail.com>
2025-04-11 17:16:46 +0900

    [#6] Add ServiceGrpcFileReceiver class

--- Apps.SKD.GrpcFileReceiver/Program.cs
+++ Apps.SKD.GrpcFileReceiver/Program.cs
@@ -51,6 +51,7 @@ namespace Apps.SKD.GrpcFileReceiver
             SeriHelper.Info("MAIN", "Configure the HTTP request pipeline.");
             SeriHelper.Info("MAIN", "Map GreeterService to service");
             app.MapGrpcService<GreeterService>();
+            app.MapGrpcService<ServiceGrpcFileReceiver>();
             app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
             app.Run();
---
+++ Apps.SKD.GrpcFileReceiver/Services/ServiceGrpcFileReceiver.cs
@@ -0,0 +1,52 @@
+﻿using Grpc.Core;
+using System.IO;
+using System.Threading.Tasks;
+using Apps.SKD.GrpcFileReceiver.Protos;
+
+namespace Apps.SKD.GrpcFileReceiver.Services;
+
+public class ServiceGrpcFileReceiver : GrpcFileService.GrpcFileServiceBase
+{
+    private readonly ILogger<ServiceGrpcFileReceiver> _logger;
+    
+    public ServiceGrpcFileReceiver(ILogger<ServiceGrpcFileReceiver> logger)
+    {
+        _logger = logger;
+    }
+    public override async Task<UploadStatus> UploadFile(
+        IAsyncStreamReader<FileUploadRequest> requestStream,
+        ServerCallContext context)
+    {
+        string fileName = null;
+        FileStream outputFile = null;
+
+        try
+        {
+            while (await requestStream.MoveNext())
+            {
+                var request = requestStream.Current;
+
+                if (fileName == null)
+                {
+                    fileName = request.FileName;
+                    outputFile = File.Create(fileName);
+                }
+
+                if (request.Content.Length > 0)
+                {
+                    await outputFile.WriteAsync(request.Content.ToByteArray());
+                }
+            }
+
+            return new UploadStatus { Status = "File uploaded successfully" };
+        }
+        finally
+        {
+            if (outputFile != null)
+            {
+                await outputFile.DisposeAsync();
+            }
+        }
+    }
+
+}
