77a128e8aec8d90b487a7d1f2c61c82fde6d71fe
송경동(SKD-PC3) <kdsong69@gmail.com>
2025-04-11 15:07:36 +0900

    [#3] Add serilog and dependency injection functionality to Apps.SKD.GrpcFileReceiver project

--- Apps.SKD.GrpcFileReceiver/Program.cs
+++ Apps.SKD.GrpcFileReceiver/Program.cs
@@ -1,4 +1,6 @@
 using Apps.SKD.GrpcFileReceiver.Services;
+using Serilog;
+using SKD.Utility;
 
 namespace Apps.SKD.GrpcFileReceiver
 {
@@ -6,18 +8,84 @@ namespace Apps.SKD.GrpcFileReceiver
     {
         public static void Main(string[] args)
         {
+            // appsettings handling
+            // decide which service objects are created and runned after reading JSON configuration file.
+            IConfigurationBuilder configBuilder = new ConfigurationBuilder();
+            buildConfig(configBuilder);
+
+            IConfiguration tempConfig = configBuilder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+
+            // creating app_data folder and its child folders, get log file name from them
+            FolderConfigHelper folderConfigHelper = new FolderConfigHelper(tempConfig);
+            string logFilePath = folderConfigHelper.LogFileName;
+
+
+            // creating logger object
+            int keepDays = tempConfig.GetValue<int>("LogRetentionDays");
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configBuilder.Build())
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File(logFilePath, rollingInterval: RollingInterval.Day, retainedFileCountLimit: keepDays)
+                .CreateLogger();
+            SeriHelper.Info("MAIN", "------------------------------------------------------");
+            SeriHelper.Info("MAIN", "Apps.SKD.GrpcFileReceiver Program Started");
+            SeriHelper.Info("MAIN", "------------------------------------------------------");
+
+
+            SeriHelper.Info("MAIN", "Building WebApplication");
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            SeriHelper.Info("MAIN", "Add gRPC service");
             builder.Services.AddGrpc();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
+            SeriHelper.Info("MAIN", "Configure the HTTP request pipeline.");
+            SeriHelper.Info("MAIN", "Map GreeterService to service");
             app.MapGrpcService<GreeterService>();
             app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
             app.Run();
+        }
+        static private void buildConfig(IConfigurationBuilder builder)
+        {
+            // EXE가 위치한 폴더에서 appsettings.json 파일을 찾을 후 읽
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+                .AddEnvironmentVariables();
+        }
+        static private void createServiceGrpcFileReceiver(IConfiguration configuration, IServiceProvider serviceProvider)
+        {
+            bool isCreateMainMenu = configuration.GetSection("ServiceGrpcFileReceiver Configuration").GetValue<bool>("IsCreateService");
+            bool isRunMainMenu = configuration.GetSection("ServiceGrpcFileReceiver Configuration").GetValue<bool>("IsRunService");
+            bool isShowMenuMainMenu = configuration.GetSection("ServiceGrpcFileReceiver Configuration").GetValue<bool>("IsShowMenu");
+
+            if (isCreateMainMenu)
+            {
+                SeriHelper.Info("MAIN", "Creating ServiceGrpcFileReceiver object");
+                //var ServiceGrpcFileReceiver = ActivatorUtilities.CreateInstance<ServiceGrpcFileReceiver>(serviceProvider);
+                //if (isRunMainMenu)
+                //{
+                //    SeriHelper.Info("MAIN", "Running ServiceGrpcFileReceiver object");
+                //    ServiceGrpcFileReceiver.Run();
+                //}
+                //if (isShowMenuMainMenu)
+                //{
+                //    SeriHelper.Info("MAIN", "Show Menu of ServiceGrpcFileReceiver object");
+                //    ServiceGrpcFileReceiver.ShowMenu();
+                //}
+                SeriHelper.Info("MAIN", "");
+            }
+            Thread.Sleep(500);
         }
     }
 }
